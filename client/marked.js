c7af0229acfa6226b30c5df2470663d18fb6dd59
Christian Stewart <kidovate@gmail.com>
2014-12-07 21:35:45 -0800

    Links open in a new tab

--- client/marked.js
+++ client/marked.js
@@ -1,1 +1,22 @@
 marked.Renderer.prototype.paragraph = function(text){ return '<p class="flow-text">' + text + '</p>\n'; }
+marked.Renderer.prototype.link =
+function (href, title, text) {
+  if (this.options.sanitize) {
+    try {
+      var prot = decodeURIComponent(unescape(href))
+        .replace(/[^\w:]/g, '')
+        .toLowerCase();
+    } catch (e) {
+      return '';
+    }
+    if (prot.indexOf('javascript:') === 0) {
+      return '';
+    }
+  }
+  var out = '<a target="_blank" href="' + href + '"';
+  if (title) {
+    out += ' title="' + title + '"';
+  }
+  out += '>' + text + '</a>';
+  return out;
+}
